aa12529de80bd94e2db39c1503884db26465c122
ms bel <msbel5@gmail.com>
2018-03-26 17:27:49 +0300

    new handle error

--- CSSimApp.UI/Global.asax.cs
+++ CSSimApp.UI/Global.asax.cs
@@ -27,13 +27,5 @@ namespace CSSimApp.UI
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
 
-        protected void Application_Error()
-        {
-            Exception ex = Server.GetLastError();
-            if (ex is ThreadAbortException)
-                return;
-
-            Response.Redirect("Error.cshtml");
-        }
     }
 }
